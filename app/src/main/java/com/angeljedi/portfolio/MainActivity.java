bf5b5de9e5e618bee818b55d2f2cc0fdaffa2a2f
Brett Campbell <angeljedi@gmail.com>
2015-06-03 10:49:03 -0600

    Added a toast with a test message when clicking on a button.

--- app/src/main/java/com/angeljedi/portfolio/MainActivity.java
+++ app/src/main/java/com/angeljedi/portfolio/MainActivity.java
@@ -1,9 +1,11 @@
 package com.angeljedi.portfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
